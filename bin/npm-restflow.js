11d12824c4f8076ab337e318b958774ce5f1e353
alfredo-monteiro <alfredo.monteiro@platum.io>
2016-09-29 16:13:47 -0100

    Command line option to set config path

--- bin/npm-restflow.js
+++ bin/npm-restflow.js
@@ -3,13 +3,30 @@
 var exec = require('child_process').exec;
 var path = require('path');
 var appRoot = require('app-root-dir').get();
-var child;
+var program = require('commander');
+var child, configPath;
 
-console.log(appRoot)
+var restFlowCommand = '/usr/bin/java -jar ' + path.resolve(__dirname, '../lib/restflow.jar');
 
-// console.log('PATH: ', path.resolve(__dirname, 'lib/restflow') );
 
-child = exec('/usr/bin/java -jar ' + path.resolve(__dirname, 'lib/restflow') + '',
+program
+  .version('0.0.1')
+  .option('-c, --config', 'Path to the config folder')
+  .parse(process.argv);
+
+
+if (process.config) {
+  configPath = appRoot + path;
+  restFlowCommand +=  ' -config ' + configPath;
+
+
+  console.log('Resolve Path',  path.resolve(path) );
+  console.log('Path config', configPath);
+  console.log('Final Command', restFlowCommand);
+}
+
+
+child = exec(restFlowCommand,
   function (error, stdout, stderr){
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
@@ -17,3 +34,24 @@ child = exec('/usr/bin/java -jar ' + path.resolve(__dirname, 'lib/restflow') + '
       console.log('exec error: ' + error);
     }
 });
+
+
+
+
+child.stdout.on('data', function(data) {
+    console.log('[RESTFLOW] ', data.toString());
+});
+
+child.stderr.on('data', function(data) {
+    console.log('[RESTFLOW-ERR]' + data);
+});
+child.on('close', function(code) {
+    console.log('[CLOSING] RestFlow: ' + code);
+});
+
+
+// http://krasimirtsonev.com/blog/article/Nodejs-managing-child-processes-starting-stopping-exec-spawn
+
+// child.stdout.on('data', function(data) {
+//     console.log('stdout: ' + data);
+// });
